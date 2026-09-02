9947a13688238b02b4c47236910e755ff87a82c8
sj23-dot <santiiago650@gmail.com>
2026-09-02 14:41:28 -0700

    Configure Next.js

---
+++ next.config.js
@@ -0,0 +1,15 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+  },
+  swcMinify: true,
+  reactStrictMode: true,
+}
+
+module.exports = nextConfig
